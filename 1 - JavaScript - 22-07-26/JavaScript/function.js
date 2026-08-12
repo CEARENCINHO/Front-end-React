f922dd1c8cf06ef4b9e221d7ff6fafe85091294f
CEARENCINHO <johnmarley126@gmail.com>
2026-08-12 15:00:04 -0400

    function

---
+++ 1 - JavaScript - 22-07-26/JavaScript/function.js
@@ -0,0 +1,12 @@
+function saudacao(nome){
+    console.log('Ola ', nome)
+}
+
+saudacao('John') 
+
+function calcularDobroNumero(num){
+    return num * 2;
+}
+
+const dobroNumber = calcularDobroNumero(4);
+console.log('O drobro do numero 4 é', dobroNumber)

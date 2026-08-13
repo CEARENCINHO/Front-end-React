153216e14de98e3eafbaf42123ec5b05fc7f0624
CEARENCINHO <johnmarley126@gmail.com>
2026-08-13 17:38:36 -0400

    função hof

--- 1 - JavaScript - 22-07-26/JavaScript/function.js
+++ 1 - JavaScript - 22-07-26/JavaScript/function.js
@@ -1,12 +1,20 @@
-function saudacao(nome){
-    console.log('Ola ', nome)
+// function saudacao(nome){
+//     console.log('Ola ', nome)
+// }
+
+// saudacao('John') 
+
+// function calcularDobroNumero(num){
+//     num * 2;
+// }
+
+// const dobroNumber = calcularDobroNumero(4);
+// console.log('O drobro do numero 4 é', dobroNumber)
+
+function pessoa(){
+    return console.log(this.name)
 }
 
-saudacao('John') 
+name = 'John'
 
-function calcularDobroNumero(num){
-    return num * 2;
-}
-
-const dobroNumber = calcularDobroNumero(4);
-console.log('O drobro do numero 4 é', dobroNumber)
+pessoa()

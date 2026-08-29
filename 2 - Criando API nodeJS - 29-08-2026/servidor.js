5519984ee57bea4e78eeeab37236aeb3635bf5cf
CEARENCINHO <johnmarley126@gmail.com>
2026-08-29 17:05:01 -0400

    Criando rotas

--- 2 - Criando API nodeJS - 29-08-2026/servidor.js
+++ 2 - Criando API nodeJS - 29-08-2026/servidor.js
@@ -3,10 +3,16 @@ import http from "http";
 // Porta de comunicação
 const PORT = 3000 
 
+// criando rotas
+const rotas = {
+    '/':'Curso de Node.js',
+    '/livros':'Entrei na rota livros',
+    '/autores':'Entrei na rota autores'
+}
 
 const server = http.createServer((req,res) => {
     res.writeHead(404,{'content-type':'text/plain'})
-    res.end('Curso de Node.js')
+    res.end(rotas[req.url])
 })
 
 server.listen(PORT, () => {

f327791a981ff3b4a4230fd28fa82d453145601d
CEARENCINHO <johnmarley126@gmail.com>
2026-08-29 13:31:53 -0400

    treinando

--- 1 - JavaScript - 22-07-26/JavaScript/att-02.js
+++ 1 - JavaScript - 22-07-26/JavaScript/att-02.js
@@ -1,33 +1,33 @@
-// // Crie uma função chamada saudacao que exibe a mensagem "Olá! Seja bem-vindo(a)!" ao ser chamada.
+// Crie uma função chamada saudacao que exibe a mensagem "Olá! Seja bem-vindo(a)!" ao ser chamada.
 
-// function saudacao(){
-//     return console.log('Opa, tudo bem?')
-// }
-// saudacao()
+function saudacao(){
+    return console.log('Opa, tudo bem?')
+}
+saudacao()
 
-// // Crie uma função apresentarPessoa(nome, idade) que exibe no console: "Olá, meu nome é [nome] e tenho [idade] anos."
-// const readline = require('readline')
+// Crie uma função apresentarPessoa(nome, idade) que exibe no console: "Olá, meu nome é [nome] e tenho [idade] anos."
+const readline = require('readline')
 
-// const leitor = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-// leitor.question('Qual é seu nome?', (nome) => {
-//     leitor.question("Quantos anos você tem?", (idade) => {
-//         if (idade >= 18){
-//             console.log((nome),"já é de maior, e esta com",(idade))
-//         } else {
-//             console.log((nome),"ainda menor de idade, e esta com",(idade))
-//         }
-//         leitor.close()
-//     })
-// })
+leitor.question('Qual é seu nome?', (nome) => {
+    leitor.question("Quantos anos você tem?", (idade) => {
+        if (idade >= 18){
+            console.log((nome),"já é de maior, e esta com",(idade))
+        } else {
+            console.log((nome),"ainda menor de idade, e esta com",(idade))
+        }
+        leitor.close()
+    })
+})
 
 
 
-// // Crie uma função chamada calcularIMC que receba dois parâmetros: peso e altura.
-// // A função deve calcular o IMC utilizando a fórmula:
+// Crie uma função chamada calcularIMC que receba dois parâmetros: peso e altura.
+// A função deve calcular o IMC utilizando a fórmula:
 
 function imc(alt,ps){
     let imcResult = ps / (alt * alt)
@@ -35,12 +35,12 @@ function imc(alt,ps){
 }
 
 const readLine = require("readline")
-const leitor = readLine.createInterface({
+const read = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-leitor.question("Qual é sua altura?",(altura) => {
+read.question("Qual é sua altura?",(altura) => {
     leitor.question("Qual é se peso?",(peso) => {
         imc(altura,peso)
         leitor.close()
